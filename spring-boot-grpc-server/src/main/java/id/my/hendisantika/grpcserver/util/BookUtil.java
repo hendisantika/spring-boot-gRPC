4e9181671b3b467a7b5dac15807bbfd1cf5a6b52
hendisantika <hendisantika@yahoo.co.id>
2024-06-14 09:36:01 +0700

    Add BookUtil🫸🌀✏️📗🐧🐳

---
+++ spring-boot-grpc-server/src/main/java/id/my/hendisantika/grpcserver/util/BookUtil.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.grpcserver.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-gRPC
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/14/24
+ * Time: 09:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class BookUtil {
+}
